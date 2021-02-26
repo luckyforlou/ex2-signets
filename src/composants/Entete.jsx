fa197c7d4b01d8f51971fb25813a6f8c5322b0e0
Sabrina Ales <sabrina-ales@hotmail.com>
2021-02-26 13:19:58 -0500

    ajout avatar

--- src/composants/Entete.jsx
+++ src/composants/Entete.jsx
@@ -1,11 +1,12 @@
 import './Entete.scss'
 import avatar from '../images/avatar.png';
+import Avatar from '@material-ui/core/Avatar';
 
 export default function Entete() {
   return (
     <header className="Entete">
       <div className="logo">Signets</div>
-      <div className="avatar">Sabrina Ales</div> <img src={avatar} alt="Camille Semaan" />
+      <div className="avatar">Sabrina Ales</div> <Avatar  src={avatar} alt="Camille Semaan" />
     </header>
   );
 }
